6eabfcfe53c6727c0e1f43b18d1f33e31b55529e
divertome <hans9645@gmail.com>
2021-05-29 18:53:32 +0900

    brunch page puppeteer

--- brunch.js
+++ brunch.js
@@ -1,3 +1,4 @@
+const { prependTo } = require('cheerio/lib/api/manipulation');
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -7,7 +8,27 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://brunch.co.kr/search');
   await page.click('input.txt_search');
-  await page.screenshot({ path: 'brunch.png' });
+  await page.keyboard.type('IT');
+  await page.keyboard.press('Enter');
+//   await page.screenshot({ path: 'brunch.png' });
 
-  await browser.close();
+  //검색완료
+  //마우스를 스크롤해서 밑으로 내린다.
+  // 키보드 화살표 아래를 눌러서 화면을 아래로 내린다.
+  //=> 무한 스크롤 게시글 데이터가 들어오지 않을까?
+  //입력장치를 사용하지 말고, 자바스크립트 내장함수를 이용해서 뷰포트의 heigth를 계속해서 내리면 될 것.
+  await page.waitForNavigation();
+  let infiniteScrollInterval = setInterval(async()=>{
+       await page.evaluate(()=>{
+    window.scrollBy(0,window.innerHeight);  
+  });//현재는 굳이 따지자면 node.js 즉 서버이기때문에 바로 window를 호출할 수 없다. 
+  //따라서 page내의 evaluate 함수를 통해서 
+  },1000);
+
+  setTimeout(async()=>{
+    clearInterval(infiniteScrollInterval);
+    console.log("done");
+    await browser.close();
+  },1000*10);
+
 })();
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+  await page.screenshot({ path: 'brunch.png' });
