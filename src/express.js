01405d37493dfd78e33ddddc1220357a9bf5113c
divertome <hans9645@gmail.com>
2021-10-20 01:15:45 +0900

    1019 express.js router practice

--- src/express.js
+++ src/express.js
@@ -1,23 +1,49 @@
 // @ts-check
 
+// const { default: bodyParser } = require('body-parser')
 const express = require('express')
 
 const app = express()
 
+const userRouter = express.Router()
+
 const PORT = 5000
-app.use('/', (req, res, next) => {
-  console.log('middleware 1')
-  const requestAt = new Date()
+
+app.use(express.json())
+
+userRouter.get('/', (req, res) => {
+  console.log('user list')
+  res.send(`user list`)
+})
+const USER = {
+  15: {
+    nickname: 'foo',
+  },
+}
+userRouter.param('id', (req, res, next, value) => {
+  console.log(`id parameter`, value)
   // @ts-ignore
-  req.requestAt = requestAt
+  req.user = USER[value]
   next()
 })
-
-app.use((req, res) => {
-  console.log('Middleware2')
+userRouter.get('/:id', (req, res) => {
+  console.log('user info with ID')
   // @ts-ignore
-  res.send(`Hello, express Requested at ${req.requestAt}`)
+  res.send(req.user)
 })
+
+userRouter.post('/:id/nickname', (req, res) => {
+  //Register user
+  // @ts-ignore
+  const { user } = req
+  const { nickname } = req.body
+
+  user.nickname = nickname
+
+  res.send(`user nickname updated : ${nickname}`)
+})
+
+app.use('/users', userRouter)
 
 app.listen(PORT, () => {
   console.log(`The express server is listening at port: ${PORT}`)
